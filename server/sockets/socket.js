800c00e428d5319410dc042276697236150c3276
samzerox <sam.zerox88@gmail.com>
2019-03-12 17:28:22 -0600

    Aplicacion Terminada

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -20,8 +20,30 @@ io.on('connection', (client) => {
     //emitir un evento llamado estadoActual
 
     client.emit('estadoActual', {
-        actual: ticketControl.getUltimoTicket()
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
     });
+
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        // actualizar notificar cambios en los ultimos 4
+
+        // emitir 'ultimos4'
+
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4()
+        });
+    })
 
 
 
